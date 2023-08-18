28f5c4713b433b229e136d7402a02dc473b40a57
mohdsajeed <mohdsajeed871@gmail.com>
2023-08-18 18:28:59 +0530

    Updates

---
+++ precache-manifest.38dc8c94219766132dcbebc9619ade53.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "30d0e17d3ac82f63ef2d",
+    "url": "/reactlivewebsite/static/css/main.648ae1fb.chunk.css"
+  },
+  {
+    "revision": "30d0e17d3ac82f63ef2d",
+    "url": "/reactlivewebsite/static/js/main.30d0e17d.chunk.js"
+  },
+  {
+    "revision": "b11a28572c024a599055",
+    "url": "/reactlivewebsite/static/css/1.de93c24f.chunk.css"
+  },
+  {
+    "revision": "b11a28572c024a599055",
+    "url": "/reactlivewebsite/static/js/1.b11a2857.chunk.js"
+  },
+  {
+    "revision": "18a195a03cc8ea014908",
+    "url": "/reactlivewebsite/static/js/runtime~main.18a195a0.js"
+  },
+  {
+    "revision": "553d70dabd1518627372641a56a69867",
+    "url": "/reactlivewebsite/static/media/img2.553d70da.png"
+  },
+  {
+    "revision": "3732f592abf9572df73ea659855bfd4a",
+    "url": "/reactlivewebsite/static/media/web.3732f592.jpg"
+  },
+  {
+    "revision": "9eae2f5bdb3e00e444a25e349c8622e9",
+    "url": "/reactlivewebsite/static/media/app.9eae2f5b.jpg"
+  },
+  {
+    "revision": "d5519b21eb449503177a5cbdb977513b",
+    "url": "/reactlivewebsite/static/media/digital.d5519b21.jpg"
+  },
+  {
+    "revision": "feffde8c801baef441ce68c7f1813a82",
+    "url": "/reactlivewebsite/static/media/marketing.feffde8c.jpeg"
+  },
+  {
+    "revision": "a129308e0e8988d4a34a9cd56257d01d",
+    "url": "/reactlivewebsite/static/media/software.a129308e.jpg"
+  },
+  {
+    "revision": "86b096145cae73859b2e2c4700d2e16c",
+    "url": "/reactlivewebsite/static/media/s1.86b09614.png"
+  },
+  {
+    "revision": "fcd8ee43bc467a5e69feff5545aea548",
+    "url": "/reactlivewebsite/index.html"
+  }
+];
